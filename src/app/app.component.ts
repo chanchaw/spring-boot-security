04b06271ce5e71c095eda171c60db5445a1e8926
chanchaw <chan_chaw@126.com>
2020-02-21 15:37:40 +0800

    SpringSecurity权限管理测试成功

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +14,9 @@ export class AppComponent {
 
   url = {
     'login':'apidata/login',
-    'getUser':'apidata/selectSpeUsers'
+    'getUser':'apidata/selectSpeUsers',
+    'selectAll':'apidata/contactCompany/selectAll',
+    'insert':'apidata/contactCompany/insert'
   }
   submitForm(): void {
     for (const i in this.validateForm.controls) {
@@ -22,15 +24,29 @@ export class AppComponent {
       this.validateForm.controls[i].updateValueAndValidity();
     }
 
-    const username:string = this.validateForm.controls.userName.value
+    const username:string = this.validateForm.controls.userName.value;
     const password:string = this.validateForm.controls.password.value;
     console.log(username +'='+password);
-    this.http.post(this.url.login,{ 'username': username,'password':password }).subscribe(
-      ( jr:JsonResult ) => console.log(jr),error => console.log('登录失败！')
-    )
+    
+    // 不能经验主义，本案例在2020年2月21日15:20:33制作
+    // 以前做过 springSecurity 的案例，采用的是post 方法时，参数在体中传递
+    // 此次一开始沿用之前的方法，后端一直接受不到登录的参数
+    // 后来网上搜索到说是拼接到 url 后传递，果然成功。
+    const url:string = this.url.login + '?username=' + username + '&password=' + password;
+    this.http.post(url,null).subscribe(
+    ( jr:JsonResult ) => console.log(jr),error => console.log('登录失败！')
+  )
+
+    // let headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' });
+    // this.http.post('apidata/login',
+    //   {'headers':headers,'params':{'username':username,'password':password}}).subscribe(
+    //   ( jr:JsonResult ) => console.log(jr),error => console.log('登录失败！')
+    // )
   }
 
-  constructor(private fb: FormBuilder,private http:HttpClient) {}
+  constructor(
+    private fb: FormBuilder,
+    public http:HttpClient) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
@@ -46,6 +62,33 @@ export class AppComponent {
       ( jr:JsonResult ) =>{
         console.log(jr);
       },error => console.log('请求获取用户信息时出现异常！')
+    )
+  }
+
+  clickSelectAll(){
+    this.http.get(this.url.selectAll).subscribe(
+      ( jr:JsonResult ) =>{
+        if( jr.code !==1 ){
+          alert(jr.msg);
+          return 
+        }
+
+        alert('查询成功！');
+      },error => console.log(error)
+    )
+
+  }
+
+  clickInsert(){
+    this.http.get(this.url.insert).subscribe(
+      ( jr:JsonResult ) =>{
+        if( jr.code !==1 ){
+          alert(jr.msg);
+          return 
+        }
+
+        alert('新增成功！');
+      },error => console.log(error)
     )
   }
 }
@@ -69,4 +112,7 @@ export class JsonResult{
       this.code = 1;
       this.data = data;
   }
+
+
+
 }
